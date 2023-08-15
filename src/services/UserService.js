92aa4621a0945a9e05266ec579b530b22304b46c
levantoann <lvtoan.19it4@vku.udn.vn>
2023-08-15 21:17:39 +0700

    update user

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -42,12 +42,8 @@ export const logoutUser = async () => {
     return res.data
 }
 
-export const updateUser = async (id ,data, access_token) => {
-    const res = await axiosJWT.put(`${process.env.REACT_APP_API_URL}/user/update-user/${id}`, data,{
-        headers: {
-            token: `Bearer ${access_token}`,
-        }
-    })
+export const updateUser = async (id ,data) => {
+    const res = await axiosJWT.put(`${process.env.REACT_APP_API_URL}/user/update-user/${id}`, data)
     return res.data
 }
 
